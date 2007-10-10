5057e76a11abd02e83b7d3d3171c4b68d9c88480
Tom Preston-Werner <tom@mojombo.com>
2007-10-10 00:18:20 -0700

    clean up heads test

--- test/test_grit.rb
+++ test/test_grit.rb
@@ -5,17 +5,25 @@ class TestGrit < Test::Unit::TestCase
     @g = Grit.new(GRIT_REPO)
   end
   
+  # descriptions
+  
   def test_description
     assert_equal "Grit is a ruby library for interfacing with git repositories.", @g.description
   end
   
-  def test_heads
+  # heads
+  
+  def test_heads_should_return_array_of_head_objects
+    @g.heads.each do |head|
+      assert_equal Grit::Head, head.class
+    end
+  end
+  
+  def test_heads_should_populate_head_data
     @g.expects(:git).returns("634396b2f541a9f2d58b00be1a07f0c358b999b3 refs/heads/master \
     initial grit setup\0Tom Preston-Werner <tom@mojombo.com> 1191997100 -0700")
     
-    heads = @g.heads
-    head = heads.first
-    assert_equal Grit::Head, head.class
+    head = @g.heads.first
     
     assert_equal '634396b2f541a9f2d58b00be1a07f0c358b999b3', head.id
     assert_equal 'refs/heads/master', head.name
@@ -23,6 +31,8 @@ class TestGrit < Test::Unit::TestCase
     assert_equal 'Tom Preston-Werner <tom@mojombo.com>', head.committer
     assert_equal Time.at(1191997100), head.date
   end
+  
+  # git
   
   def test_git
     assert_match /^git version [\d\.]*$/, @g.git('version')
