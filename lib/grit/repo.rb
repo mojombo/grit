2adfc816945c1ea2c0f147eef8e46dc3b8596b35
Johan Sørensen <johan@johansorensen.com>
2009-02-13 12:03:38 +0100

    Added Repo#commit_deltas_from as a (fairly expensive and lazy) way of getting the commits that differ between two different repositories

--- lib/grit/repo.rb
+++ lib/grit/repo.rb
@@ -209,6 +209,20 @@ module Grit
       Commit.find_all(self, id, options).first
     end
     
+    # Returns a list of commits that is in +other_repo+ but not in self
+    #
+    # Returns Grit::Commit[]
+    def commit_deltas_from(other_repo, ref = "master", other_ref = "master")
+      # TODO: we should be able to figure out the branch point, rather than
+      # rev-list'ing the whole thing
+      repo_refs       = self.git.rev_list({}, ref).strip.split("\n")
+      other_repo_refs = other_repo.git.rev_list({}, other_ref).strip.split("\n")
+      
+      (other_repo_refs - repo_refs).map do |ref|
+        Commit.find_all(other_repo, ref, {:max_count => 1}).first
+      end
+    end
+    
     # The Tree object for the given treeish reference
     #   +treeish+ is the reference (default 'master')
     #   +paths+ is an optional Array of directory paths to restrict the tree (deafult [])
