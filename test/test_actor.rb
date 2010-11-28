1cd107b7a3beec495f48543b838bba057e725bb7
Ben Hughes <ben@pixelmachine.org>
2010-11-28 12:46:41 -0500

    Fixed time zone offset test for the East Coast

--- test/test_actor.rb
+++ test/test_actor.rb
@@ -9,7 +9,10 @@ class TestActor < Test::Unit::TestCase
   def test_output_adds_tz_offset
     t = Time.now
     a = Actor.new("Tom Werner", "tom@example.com")
-    assert_equal "Tom Werner <tom@example.com> #{t.to_i} -0700", 
+    
+    sign, zone = (t.gmt_offset < 0) ? ["-", -1*t.gmt_offset] : ["+", t.gmt_offset]
+    offset = "%s%02d%02d" % [sign, zone/3600, zone/3600/60]
+    assert_equal "Tom Werner <tom@example.com> #{t.to_i} #{offset}", 
       a.output(t)
   end
 
