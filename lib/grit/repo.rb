90d9479b8243f188a38b6dccc0257f475f5b5fb9
Kyle Fuller <inbox@kylefuller.co.uk>
2011-02-10 01:38:39 +0000

    Add a method to a repo so you can perform git hooks

--- lib/grit/repo.rb
+++ lib/grit/repo.rb
@@ -200,6 +200,17 @@ module Grit
       self.git.fs_read('description').chomp
     end
 
+    # Execute a hook
+    #
+    # name - The name of the hook as a String
+    def hook(name, timeout = 10)
+      file = File.join(self.git.git_dir, 'hooks', name)
+
+      if File.executable?(file)
+        Grit::Process.new(file, {}, { :timeout => timeout })
+      end
+    end
+
     def blame(file, commit = nil)
       Blame.new(self, file, commit)
     end
