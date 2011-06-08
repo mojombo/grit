0822ea6aeef2fc0807688c5b7f0161c1c0577655
Aman Gupta <aman@tmm1.net>
2011-06-08 10:59:54 -0700

    remove all the grit jruby hacks in favor of updated posix-spawn

--- lib/grit.rb
+++ lib/grit.rb
@@ -48,11 +48,6 @@ require 'grit/submodule'
 require 'grit/blame'
 require 'grit/merge'
 
-# platform specific requires
-if defined?(RUBY_ENGINE) && RUBY_ENGINE =~ /jruby/
-  require 'grit/jruby'
-end
-
 module Grit
   VERSION = '2.4.1'
 
