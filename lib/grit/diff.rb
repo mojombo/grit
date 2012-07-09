9d7c3e6b9480013f97308da1b0054f2224fbf8f9
Nick Hengeveld <nickh@github.com>
2012-07-09 16:58:16 -0700

    Use CharlockHolmes to detect encoding/transcode to UTF-8
    For compatibility with both Ruby 1.8 and 1.9.

--- lib/grit/diff.rb
+++ lib/grit/diff.rb
@@ -1,3 +1,6 @@
+require 'charlock_holmes'
+require 'charlock_holmes/string'
+
 module Grit
 
   class Diff
@@ -31,8 +34,8 @@ module Grit
       while !lines.empty?
         m, q1, a_path, q2, b_path = *lines.shift.match(%r{^diff --git ("?)a/(.+?)\1 ("?)b/(.+?)\3$})
 
-        a_path = a_path.gsub(/\\([0-9]{3})/){|c| c[1..-1].oct.chr}.force_encoding('UTF-8')
-        b_path = b_path.gsub(/\\([0-9]{3})/){|c| c[1..-1].oct.chr}.force_encoding('UTF-8')
+        a_path = transcode( a_path.gsub(/\\([0-9]{3})/){|c| c[1..-1].oct.chr} )
+        b_path = transcode( b_path.gsub(/\\([0-9]{3})/){|c| c[1..-1].oct.chr} )
 
         if lines.first =~ /^old mode/
           m, a_mode = *lines.shift.match(/^old mode (\d+)/)
@@ -77,6 +80,22 @@ module Grit
 
       diffs
     end
+
+    private
+
+      def self.transcode(string, target_encoding = 'UTF-8')
+        detected = string.detect_encoding
+
+        if detected.nil? || detected[:encoding].nil? || detected[:encoding] == target_encoding
+          string
+        else
+          begin
+            CharlockHolmes::Converter.convert( string, detected[:encoding], target_encoding )
+          rescue
+            string
+          end
+        end
+      end
   end # Diff
 
 end # Grit
