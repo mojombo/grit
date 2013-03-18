7671d15140e1c8bf4cb0c0087bed45bebeabe09a
Harry Cheung <harry.cheung@gmail.com>
2013-03-18 16:32:25 -0700

    Patched for Ruby 2.0.0 support.

--- lib/grit/ruby.rb
+++ lib/grit/ruby.rb
@@ -1,5 +1,5 @@
 class String
-  if ((defined? RUBY_VERSION) && (RUBY_VERSION[0..2] == "1.9"))
+  if ((defined? RUBY_VERSION) && (RUBY_VERSION[0..2].to_f >= 1.9))
     def getord(offset); self[offset].ord; end
   else
     alias :getord :[]
